f4182704cec706b5af870739a9d063dda9342db8
Rui Nascimento <sruy19@gmail.com>
2021-09-01 22:59:41 -0300

    Operadores aritméticos

---
+++ operations.php
@@ -0,0 +1,34 @@
+<?php
+$idade = 24;
+$idadeDaqui10Anos = $idade + 10;
+$idadeHa5Anos = $idade - 5;
+echo $idadeDaqui10Anos;
+echo "\n";
+echo $idadeHa5Anos;
+echo "\n";
+//Operações Aritiméticas
+/*
+ * Soma : +
+ * Subtração: -
+ * Multiplicação: *
+ * Divisão: /
+ * Potência: **
+ * MOD: %
+ */
+
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$potencia = 2**5;
+$restoDaDivisao = 10 % 2;
+echo "\nSoma:\n";
+echo $soma;
+echo "\nSubtração:\n";
+echo $subtracao;
+echo "\nMultiplicação\n";
+echo $multiplicacao;
+echo "\nDivisão\n";
+echo $divisao, "\n";
+echo "Potência: ", $potencia, "\n";
+echo "MOD: ", $restoDaDivisao, "\n";
