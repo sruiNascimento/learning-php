6f8d44c33ca0cfc5acb80c588acd3b7b13b6c75c
Rui Nascimento <sruy19@gmail.com>
2021-09-01 23:34:22 -0300

    constante PHP_EOL

--- strings.php
+++ strings.php
@@ -5,8 +5,10 @@ $age = 24;
 /*
  * Concatenação de strings pode ser feita pelo . ou ,
  * Caso usar aspas duplas, basta usar o $com a variável
+ * Quebra de linha há 2 formas
+ * \n ou PHP_EOL
  */
 
 echo 'My name is ' . $name . ' I\'m ' . $age . "\n";
-echo "My name is $name I'm $age";
+echo "My name is $name I'm $age" . PHP_EOL;
 ?>
