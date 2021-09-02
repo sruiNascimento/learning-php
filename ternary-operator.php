58297cc881aac76a34399708302a99013443c4b2
Rui Nascimento <sruy19@gmail.com>
2021-09-02 10:18:17 -0300

    ternary operator - expression ? true : false

---
+++ ternary-operator.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * Operador ternário
+ * (expressão) ? true : false;
+ */
+
+$idade = 18;
+$msg = $idade >= 18 ? "Você é maior de idade" : "Você é menor de idade";
+echo $msg . PHP_EOL;
+?>
