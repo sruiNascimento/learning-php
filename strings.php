6e78d8a8a67a151d960d644bf387d315742f7a30
Rui Nascimento <sruy19@gmail.com>
2021-09-01 23:22:35 -0300

    strings e concatenação

---
+++ strings.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Rui";
+$age = 24;
+
+/*
+ * Concatenação de strings pode ser feita pelo . ou ,
+ * Caso usar aspas duplas, basta usar o $com a variável
+ */
+
+echo 'My name is ' . $name . ' I\'m ' . $age . "\n";
+echo "My name is $name I'm $age";
+?>
