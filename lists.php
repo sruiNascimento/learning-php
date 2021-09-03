1fec04eb631d08c726513e9f6a3733f64a0fee2e
Rui Nascimento <sruy19@gmail.com>
2021-09-03 08:27:19 -0300

    listas - Array

---
+++ lists.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Array
+ * dá para adicionar diferentes tipos de dados
+ * dá para acessar pelo índice
+ */
+
+$idadesList = [12, 24, 25, 21, 26];
+
+echo gettype($idadesList) . PHP_EOL;
+echo "Index 1 $idadesList[1]" . PHP_EOL;
+
+//criando um array através da função
+$filmesMarvel = array('Avengers: Endgame', 'Captain Marvel', 'Homem-formiga e a Vespa');
+
+?>
