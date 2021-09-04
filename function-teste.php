b5ece32d779af5ce77f108b63afbdd83b6824e1e
Rui Nascimento <sruy19@gmail.com>
2021-09-04 08:38:44 -0300

    Trabalhando com arquivos diferentes

---
+++ function-teste.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'funtions-of-teste.php';
+//include_once
+//include
+//require
+//Importanto arquivos
+//include - tratará como arquivo essencial, se não encontrar o arquivo ele continuará executando
+//require, irá dar erro pois o arquivo é essencil
+//caso façamos a importação duas vezes do mesmo arquivo, irá gerar um erro. Portando usamos o require/include_once
